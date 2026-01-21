fe419b1220c07c06bb659a25643051f2e5cec810
Romel Luis Faife Cruz <113552563+romeluis@users.noreply.github.com>
2026-01-21 17:27:51 -0500

    Added compact text field

---
+++ Sources/Control/Control/ControlCompactTextField.swift
@@ -0,0 +1,119 @@
+//
+// ControlCompactTextField.swift
+// Control
+//
+// Created by Romel Luis Faife Cruz on 2025-09-10.
+
+import SwiftUI
+import SwiftData
+
+public struct ControlCompactTextField: View {
+    @Binding var input: String
+    @Binding var inputState: ControlInputState
+    
+    var placeholderText: String = ""
+    var showError: Bool = true
+    
+    var backgroundColour: Color = .Control.white
+    var outlineColour: Color = .clear
+    var textColour: Color = .accentColor
+
+    @Binding var validationTrigger: Bool
+
+    var isValid: ((String) -> ControlInputState)?
+
+    public init(
+        input: Binding<String>,
+        inputState: Binding<ControlInputState>,
+        placeholderText: String = "",
+        showError: Bool = true,
+        backgroundColour: Color = .Control.white,
+        outlineColour: Color = .clear,
+        textColour: Color = .accentColor,
+        validationTrigger: Binding<Bool> = .constant(true),
+        isValid: ((String) -> ControlInputState)? = nil
+    ) {
+        self._input = input
+        self._inputState = inputState
+        self.placeholderText = placeholderText
+        self.showError = showError
+        self.backgroundColour = backgroundColour
+        self.outlineColour = outlineColour
+        self.textColour = textColour
+        self._validationTrigger = validationTrigger
+        self.isValid = isValid
+    }
+
+    public var body: some View {
+        VStack (alignment: .leading, spacing: 5) {
+            
+            //Textfield
+            HStack (spacing: 15) {
+                TextField(placeholderText, text: $input)
+                    .smallText()
+                    .padding(6)
+                    .padding(.horizontal)
+                    .foregroundColor(textColour)
+                    .backgroundStroke(cornerRadius: 20, colour: inputState != .valid && showError  ? .red : outlineColour)
+                    .backgroundFill(cornerRadius: 20, colour: backgroundColour)
+            }
+            
+            //Error message if input is invalid
+            if case let .invalid(message) = inputState, showError {
+                Text(message)
+                    .smallText()
+                    .padding(.leading, 7)
+            }
+        }
+        .onChange(of: input) {
+            self.updateInputs()
+        }
+        .onChange(of: validationTrigger) {
+            self.updateInputs()
+        }
+    }
+
+    func updateInputs() {
+        withAnimation (.spring(duration: 0.3)) {
+                if isValid != nil {
+                    inputState = isValid!(input)
+                } else {
+                    inputState = .valid
+                }
+            }
+    }
+}
+
+#Preview (traits: .controlPreview) {
+    @Previewable @State var text1: String = ""
+    @Previewable @State var text3: String = ""
+    @Previewable @State var update: Bool = false
+    @Previewable @State var valid1: ControlInputState = .valid
+    @Previewable @State var valid3: ControlInputState = .valid
+    
+    VStack {
+        ControlTextField(title: "Name", input: $text1, inputState: $valid1, placeholderText: "Computer Organization", validationTrigger: $update) { text in
+            if text.isEmpty {
+                return .invalid(message: "No empty")
+            }
+            return .valid
+        }
+        
+        ControlTextField(input: $text3, inputState: $valid3, placeholderText: "Computer Organization", validationTrigger: $update) { text in
+            if text.isEmpty {
+                return .invalid(message: "No empty")
+            } else if text == "Test" {
+                return .invalid(message: "No test")
+            }
+            return .valid
+        }
+        Spacer()
+        Text("Valid States: \(valid1 == .valid ? "✓" : "✗") \(valid3 == .valid ? "✓" : "✗")")
+        ControlButton(text: "Update Signal", type: .primary) {
+            update.toggle()
+        }
+    }
+    .padding()
+    .background(.fill)
+}
+
